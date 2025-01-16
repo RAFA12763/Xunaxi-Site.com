166db3072c9414cab7f9571a76806a06f2f81c78
RAFA12763 <143661914+RAFA12763@users.noreply.github.com>
2025-01-15 23:46:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-// Função para simular o login
+
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    // Usuário e senha pré-definidos (poderiam ser obtidos de um banco de dados, por exemplo)
     var correctUsername = "usuario";
     var correctPassword = "senha123";
 
-    // Verificando se o login está correto
     if (username === correctUsername && password === correctPassword) {
         alert("Login bem-sucedido!");
-        // Aqui você pode redirecionar o usuário para outra página
-        // window.location.href = "pagina_interna.html";
+
     } else {
         document.getElementById("error-message").style.display = "block";
     }
